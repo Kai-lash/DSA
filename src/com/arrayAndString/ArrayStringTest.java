ec787b63f174ad38211297701c2d6adaef3aa41f
Kai-lash <kailashjoshi1104@gmail.com>
2016-08-24 21:38:39 -0400

    added a function to add 1 to the array based integer

--- src/com/arrayAndString/ArrayStringTest.java
+++ src/com/arrayAndString/ArrayStringTest.java
@@ -6,8 +6,14 @@ public class ArrayStringTest {
 		for(int i : evenOdd(new int[]{1,2,3,4,5,6,7,8,9})){
 			System.out.print(i + " ");
 		}
+		
 		System.out.println();
 		for(int i : dutchFlagPartition(new int[]{1,2,7,4,5,6,5,3,1}, 4)){
+			System.out.print(i + " ");
+		}
+		
+		System.out.println();
+		for(int i : addOne(new int[]{9,8})){
 			System.out.print(i + " ");
 		}
 	}
@@ -42,6 +48,30 @@ public class ArrayStringTest {
 				array[equalIndex] = array[--moreIndex];
 				array[moreIndex] = temp;
 			}
+		}
+		return array;
+	}
+	
+	private static int[] addOne(int[] array){
+		int carry = 0, i = array.length - 1;
+		array[i] = array[i] + 1;
+		while(i >= 0) {
+			if(array[i] + carry >= 10){
+				array[i] = array[i] + carry - 10;
+				carry = 1;
+			}else{
+				array[i] = array[i] + carry;
+				carry = 0;
+			}
+			i--;
+		} 
+		if(carry == 1){
+			int[] newArray = new int[array.length + 1];
+			newArray[0] = carry;
+			for(int j = 1; j < newArray.length - 1; j++){
+				newArray[j] = array[j];
+			}
+			array = newArray;
 		}
 		return array;
 	}
