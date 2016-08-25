762951d40c463185d429f53448d0576e4e31f8d3
Kai-lash <kailashjoshi1104@gmail.com>
2016-08-24 20:23:07 -0400

    added a function to reorder array into smaller equal and larger elements

--- src/com/arrayAndString/ArrayStringTest.java
+++ src/com/arrayAndString/ArrayStringTest.java
@@ -6,6 +6,10 @@ public class ArrayStringTest {
 		for(int i : evenOdd(new int[]{1,2,3,4,5,6,7,8,9})){
 			System.out.print(i + " ");
 		}
+		System.out.println();
+		for(int i : dutchFlagPartition(new int[]{1,2,7,4,5,6,5,3,1}, 4)){
+			System.out.print(i + " ");
+		}
 	}
 	
 	private static int[] evenOdd(int[] array){
@@ -22,4 +26,23 @@ public class ArrayStringTest {
 		return array;
 	}
 
+	private static int[] dutchFlagPartition(int[] array, int pivotIndex){
+		int lessIndex = 0, moreIndex = array.length, equalIndex = 0;
+		while(equalIndex < moreIndex){
+			if(array[equalIndex] < array[pivotIndex]){
+				int temp = array[lessIndex];
+				array[lessIndex++] = array[equalIndex];
+				array[equalIndex++] = temp;
+			}
+			else if(array[equalIndex] == array[pivotIndex]){
+				equalIndex++;
+			}
+			else {
+				int temp = array[equalIndex];
+				array[equalIndex] = array[--moreIndex];
+				array[moreIndex] = temp;
+			}
+		}
+		return array;
+	}
 }
