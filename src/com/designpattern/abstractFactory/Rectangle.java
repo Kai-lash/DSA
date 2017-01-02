1598f00959052e3052b744625987f355d18e4fc3
Kai-lash <kailashjoshi1104@gmail.com>
2017-01-01 23:26:39 -0500

    added rectangle shape class

---
+++ src/com/designpattern/abstractFactory/Rectangle.java
@@ -0,0 +1,10 @@
+package com.designpattern.abstractFactory;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Drawing rectangle...");		
+	}
+
+}
