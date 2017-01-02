5eb4a538948569b720d8a9c98eac5416fe371e77
Kai-lash <kailashjoshi1104@gmail.com>
2017-01-01 23:24:42 -0500

    added factory producer class

---
+++ src/com/designpattern/abstractFactory/FactoryProducer.java
@@ -0,0 +1,13 @@
+package com.designpattern.abstractFactory;
+
+public class FactoryProducer {
+	public static AbstractFactory getFactory(String factory) {
+		if (factory.equalsIgnoreCase("SHAPE")) {
+			return new ShapeFactory();
+		} else if (factory.equalsIgnoreCase("COLOR")) {
+			return new ColorFactory();
+		} else {
+			return null;
+		}
+	}
+}
