18e0027c40b4851faba714c1befee3e2fa6c90d0
Kai-lash <kailashjoshi1104@gmail.com>
2017-01-03 00:40:55 -0500

    added student view class

---
+++ src/com/designpattern/mvc/StudentView.java
@@ -0,0 +1,8 @@
+package com.designpattern.mvc;
+
+public class StudentView {
+	public void printStudentDetails(String name, int rollno) {
+		System.out.println("Student name : " + name);
+		System.out.println("Student rollno : " + rollno);
+	}
+}
